dfb31f4dda7c9cae53322c82120fb2ab4468f239
Yevhen Malyshko <memx86@gmail.com>
2022-01-26 18:02:15 +0200

    test2

--- src/App.js
+++ src/App.js
@@ -1,79 +1,79 @@
-// import { Component, Fragment } from "react";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.min.css";
-// import Section from "components/Section";
-// import Container from "components/Container";
-// import ContactForm from "components/ContactForm";
-// import Filter from "components/Filter";
-// import ContactList from "components/ContactList";
+import { Component, Fragment } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import Section from "components/Section";
+import Container from "components/Container";
+import ContactForm from "components/ContactForm";
+import Filter from "components/Filter";
+import ContactList from "components/ContactList";
 
-// class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-//       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-//       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-//     ],
-//     filter: "",
-//   };
-//   addContact = (contact) => {
-//     this.setState(({ contacts }) => ({ contacts: [...contacts, contact] }));
-//   };
-//   removeContact = (e) => {
-//     const name = e.target.dataset.name;
-//     this.setState((prevState) => ({
-//       contacts: prevState.contacts.filter((contact) => contact.name !== name),
-//     }));
-//     toast.info(`${name} was removed from contacts`);
-//   };
-//   getNames = () => this.state.contacts.map((contact) => contact.name);
-//   setFilterState = (value) => this.setState(() => ({ filter: value }));
-//   filterContacts = () => {
-//     const contacts = this.state.contacts;
-//     const filter = this.state.filter.toLowerCase().trim();
-//     return contacts.filter((contact) =>
-//       contact.name.toLowerCase().includes(filter)
-//     );
-//   };
-//   render() {
-//     const contacts = this.filterContacts();
-//     const names = this.getNames();
-//     return (
-//       <Fragment>
-//         <Section>
-//           <Container>
-//             <h1>Phonebook</h1>
-//           </Container>
-//         </Section>
-//         <Section>
-//           <Container>
-//             <ContactForm names={names} onFormSubmit={this.addContact} />
-//           </Container>
-//         </Section>
-//         <Section>
-//           <Container>
-//             <h2>Contacts</h2>
-//             <Filter setFilter={this.setFilterState} />
-//             <ContactList
-//               contacts={contacts}
-//               removeContact={this.removeContact}
-//             />
-//           </Container>
-//         </Section>
-//         <ToastContainer
-//           position="top-right"
-//           autoClose={3000}
-//           hideProgressBar={true}
-//           newestOnTop={false}
-//           closeOnClick
-//           rtl={false}
-//           pauseOnFocusLoss
-//           draggable
-//           pauseOnHover
-//         />
-//       </Fragment>
-//     );
-//   }
-// }
-// export default App;
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+  addContact = (contact) => {
+    this.setState(({ contacts }) => ({ contacts: [...contacts, contact] }));
+  };
+  removeContact = (e) => {
+    const name = e.target.dataset.name;
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.name !== name),
+    }));
+    toast.info(`${name} was removed from contacts`);
+  };
+  getNames = () => this.state.contacts.map((contact) => contact.name);
+  setFilterState = (value) => this.setState(() => ({ filter: value }));
+  filterContacts = () => {
+    const contacts = this.state.contacts;
+    const filter = this.state.filter.toLowerCase().trim();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter)
+    );
+  };
+  render() {
+    const contacts = this.filterContacts();
+    const names = this.getNames();
+    return (
+      <Fragment>
+        <Section>
+          <Container>
+            <h1>Phonebook</h1>
+          </Container>
+        </Section>
+        <Section>
+          <Container>
+            <ContactForm names={names} onFormSubmit={this.addContact} />
+          </Container>
+        </Section>
+        <Section>
+          <Container>
+            <h2>Contacts</h2>
+            <Filter setFilter={this.setFilterState} />
+            <ContactList
+              contacts={contacts}
+              removeContact={this.removeContact}
+            />
+          </Container>
+        </Section>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </Fragment>
+    );
+  }
+}
+export default App;
